87d1c7d6fddb8620f8cc5387bc3d23eaa2c9cbac
Yuto Nakamura <maron07222010@gmail.com>
2023-06-06 23:15:06 +0900

    intをprintする関数を用意

--- kernel/main.c
+++ kernel/main.c
@@ -1,5 +1,11 @@
 # define UARTADR 0x10000000
 
+void print_int(int i) {
+    i %= 10;
+    volatile unsigned int * const UART0DR = (unsigned int *)UARTADR;
+    *UART0DR = (unsigned int)(i + '0');
+}
+
 void print_uart0(const char *s) {
     volatile unsigned int * const UART0DR = (unsigned int *)UARTADR;
     while (*s != '\0') {
@@ -10,5 +16,6 @@ void print_uart0(const char *s) {
 
 int main(){
     print_uart0("Hello world!\n");
+    print_int(1);
     return 0;
 }
